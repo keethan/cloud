3bbf72a9e82c1a7e92ad2ca0c5a40c9e90515caf
keethan <a_keetan@yahoo.co.uk>
2013-02-17 12:53:07 +0100

    @browser is changed to $

--- features/support/app_life_cycle_hooks.rb
+++ features/support/app_life_cycle_hooks.rb
@@ -2,11 +2,11 @@ require 'calabash-android/management/adb'
 require 'calabash-android/operations'
 include Calabash::Android::Operations
 
-#Before do |scenario|
+Before do |scenario|
       #$startTime = Time.now.to_f
   #start_test_server_in_background
 
-#end
+end
 
 After do |scenario|
   if scenario.failed?
